d80322d62f5fc84389c4b1a481396503770ee3d5
kevinmillo <kbullor@hotmail.com>
2021-11-20 13:00:16 +0000

    conditional profile card

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,26 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1> ${variables.name ? variables.name : "name"}
+                      ${variables.lastname ? variables.lastname : "last name"}
+              </h1>
+          <h2> ${variables.role ? variables.role : "Role"}</h2>
+          <h3>
+          ${variables.city ? variables.city : "City,"}
+          ${variables.country ? variables.country : "Country"}
+          </h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href= "https://twitter.com/${variables.twitter}"
+            ><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -60,7 +72,7 @@ window.onload = function() {
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: null,
+    twitter: "AlejandroSanchezR",
     github: "alesanchezr",
     linkedin: null,
     instagram: null,
